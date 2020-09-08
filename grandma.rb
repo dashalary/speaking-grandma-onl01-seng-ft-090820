febb6f8963d462e5cc5f5c3f56b7e4af59f4e517
Dasha Lary <dk2800@nyu.edu>
2020-09-08 17:52:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(string)
   if string.upcase
     return "NO, NOT SINCE 1938!"
-  elsif string.upcase && string == "I LOVE YOU GRANDMA!"
+  elsif string == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 elsif !string.upcase 
   return "HUH?! SPEAK UP, SONNY!"
